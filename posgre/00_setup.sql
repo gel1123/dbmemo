e36fffec8bb7f46c63abef7d96e849f96405bc24
gel1123 <atno.14@gmail.com>
2021-01-18 22:33:30 +0900

    hoge

--- posgre/00_setup.sql
+++ posgre/00_setup.sql
@@ -1,1 +1,8 @@
 create database sandbox;
+create table tbl01 (
+	id int,
+	name varchar(100)
+);
+insert into tbl01 values (1, 'okinawa');
+insert into tbl01 values (2, 'aichi');
+insert into tbl01 values (3, 'kanagawa');
