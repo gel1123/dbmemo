e369646f716cadb083c84db7ac1f1aa9cb8a3882
gel1123 <atno.14@gmail.com>
2021-02-07 07:31:06 +0900

    hoge

--- oracledb/st01.sql
+++ oracledb/st01.sql
@@ -7,4 +7,8 @@ insert into member values (1, '090-1234-5678');
 insert into member values (2, '080-1234-5678');
 insert into member values (3, '070-1234-5678');
 
-select id,  rpad(substr(phone, 0, 4), 8, '*') || lpad(substr(phone, 12, 12), 4, '*') "phone_for-cs", phone from member;
+select id, rpad(
+	substr(phone, 1, 4), 8, '*'
+) || substr(phone, 9, 9)  || lpad(
+	substr(phone, 13, 13), 4, '*'
+) "phone_for-cs", phone from member;
