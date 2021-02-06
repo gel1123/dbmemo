382e66e0d446af0adb9f94db4f4880756fab4510
gel1123 <atno.14@gmail.com>
2021-02-07 07:03:26 +0900

    hoge

---
+++ oracledb/st01.sql
@@ -0,0 +1,8 @@
+truncate table member;
+create table member (
+	id number(3),
+	phone varchar(13)
+)
+insert into member values (1, '090-1234-5678');
+insert into member values (2, '090-1234-5678');
+insert into member values (3, '090-1234-5678');
